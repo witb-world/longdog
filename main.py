972106b29cc15ad5792f1e884a065d694753947f
Eli Wald <eli.wald@nccgroup.com>
2023-08-29 10:13:14 -0700

    Make CLI option for findings that report can parse

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ SHARPHOUND_BASE_PATH = ''
 @click.option('--grouper-input', help='Path to Group3r JSONL input', required=False, type=click.Path())
 @click.option('--sharphound-dir', help='Path to directory containing SharpHound .json files', required=True, type=click.Path())
 @click.option('--output', default='longdog-out.json', help='Output file', type=str)
+@click.option('--findings-output', default='findings_output.js', help='Output file', type=str)
 @click.option('--recurse-links', help='Recursively resolve AD relatioinships for Group policy links. May degrade performance.', type=bool, default=False)
 
-def run_longdog(sharphound_dir, grouper_input, output, recurse_links):
+def run_longdog(sharphound_dir, grouper_input, output, findings_output, recurse_links):
     if grouper_input:
         GROUPER_PATH = grouper_input
         print("Loading group3r data from", GROUPER_PATH)
@@ -33,7 +34,11 @@ def run_longdog(sharphound_dir, grouper_input, output, recurse_links):
     with open(output, 'w') as mapped:
         json.dump(res, mapped)
     
-    print(policy_assessor.assess_findings(output_path=output))
+    findings = policy_assessor.assess_findings(output_path=output)
+
+    with open(findings_output, 'w') as findings_file:
+        findings_file.write(f"results = {findings}")
+
 
 if __name__ == '__main__':
     run_longdog()
