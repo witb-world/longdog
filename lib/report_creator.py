a4639b1e76f034327dfdc5a64d9698da24c64668
Eli Wald <eli.wald@nccgroup.com>
2023-09-01 14:40:32 -0700

    Update logic to process output directory

--- lib/report_creator.py
+++ lib/report_creator.py
@@ -5,22 +5,43 @@ import bs4
 import os
 import shutil
 
+from loguru import logger
+
 REPORT_TEMPLATE_PATH = "./out/view/html/report.html"
 
-DEFAULT_OUTPUT_DIR = "./results"
+DEFAULT_OUTPUT_DIR = "./results/"
 DEFAULT_HTML_OUTPUT_PATH = "ld_report.html"
 
-def produce_report(findings_file_path: str):
-    shutil.copytree('./out/view/inc-longdog', DEFAULT_OUTPUT_DIR)
-    shutil.copyfile(findings_file_path, f'{DEFAULT_OUTPUT_DIR}/{findings_file_path}')
-    with open(REPORT_TEMPLATE_PATH, 'r') as report_template:
-        report_str_data = report_template.read()
-        report_soup = bs4.BeautifulSoup(report_str_data)
-    
-    findings_data_js = report_soup.new_tag("script", src=findings_file_path)
-    report_soup.head.append(findings_data_js)
+FINDINGS_PATHNAME = 'findings_output.js'
 
-    # os.mkdir(DEFAULT_OUTPUT_DIR)
-    with open(f'{DEFAULT_OUTPUT_DIR}/{DEFAULT_HTML_OUTPUT_PATH}', "w") as output_html:
-        output_html.write(str(report_soup))
+class LongdogReport:
+    def __init__(self, report_path):
+        # n.b.: we'll probably have to parse this path to make sure the formatting works
+        # or use another library besides strings.
+        self.report_path = report_path
+        self.findings_file_path = self.report_path + FINDINGS_PATHNAME
+        self.findings_filename = FINDINGS_PATHNAME
+
+
+    def make_report_dir(self):
+        # os.mkdir(self.report_path)
+        shutil.copytree('./out/view/inc-longdog', self.report_path)
+
+    def write_findings_file(self, findings: dict):
+        logger.debug(f'Writing findings to {self.findings_file_path}')
+        with open(self.findings_file_path, 'w') as findings_file:
+            findings_file.write(f'results = {findings}')
+
+    def produce_report(self):
+        # shutil.copyfile(findings_file_path, f'{DEFAULT_OUTPUT_DIR}/{findings_file_path}')
+        with open(REPORT_TEMPLATE_PATH, 'r') as report_template:
+            report_str_data = report_template.read()
+            report_soup = bs4.BeautifulSoup(report_str_data)
+        
+        findings_data_js = report_soup.new_tag("script", src=self.findings_filename)
+        report_soup.head.append(findings_data_js)
+
+        # os.mkdir(DEFAULT_OUTPUT_DIR)
+        with open(f'{self.report_path}/{DEFAULT_HTML_OUTPUT_PATH}', "w") as output_html:
+            output_html.write(str(report_soup))
 
